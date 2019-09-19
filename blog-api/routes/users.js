9a523dcfc9eb7aa5ebc1bf71b200c2b1bdd8d5ca
huhui <569263082@qq.com>
2019-09-19 23:29:04 +0800

    初始化项目

--- blog-api/routes/users.js
+++ blog-api/routes/users.js
@@ -6,4 +6,14 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/test', function(req, res, next) {
+  res.json({
+    code:0,
+    data:{
+      name:'hit'
+    },
+    msg:'成功返回'
+  })
+});
+
 module.exports = router;
