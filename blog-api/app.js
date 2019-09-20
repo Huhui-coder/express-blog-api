64694514c684b09cd3c362b3e7565c4960a04253
huhui <569263082@qq.com>
2019-09-21 01:12:13 +0800

    添加jwt功能，并实现对option请求过滤

--- blog-api/app.js
+++ blog-api/app.js
@@ -1,36 +1,72 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var bodyParser = require("body-parser");
+var jwt = require("jwt-simple"); //引入jwt中间件
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+var articleRouter = require("./routes/article");
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
+//设置jwt密钥
+app.set("jwtTokenSecret", "qwert569263082@!");
 
+//配置body-parser中间件
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+app.use(bodyParser.json());
+
+//设置允许跨域访问该服务.
 app.all('*', function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  res.header('Access-Control-Allow-Methods', '*');
+  res.header('Access-Control-Allow-Origin', '*');  //请求源
+  res.header('Access-Control-Allow-Headers', '*');  //请求头
+  res.header('Access-Control-Max-Age',600);  //请求时间
+  res.header('Access-Control-Allow-Methods', '*');  //请求方法  
   res.header('Content-Type', 'application/json;charset=utf-8');
-  next();
+   
+  if (req.method == 'OPTIONS') {
+    res.send(200);
+  }
+  else {
+    next();
+  }
+});
+  
+
+app.all("/article", (req, res) => {
+  // 获取token,这里默认是放在headers的authorization
+  let token = req.headers.authorization;
+  if (token) {
+    let decoded = jwt.decode(token, app.get("jwtTokenSecret"));
+    // 判断是否token已过期以及接收方是否为自己
+    if (decoded.exp <= Date.now() || decoded.aud !== "hit") {
+      res.sendStatus(401);
+    } else {
+      res.sendStatus(200);
+    }
+  } else {
+    res.sendStatus(401);
+  }
 });
 
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/article", articleRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -41,11 +77,11 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
--- blog-api/routes/users.js
+++ blog-api/routes/users.js
@@ -1,19 +1,72 @@
-var express = require('express');
+var express = require("express");
+var jwt = require("jwt-simple"); //引入jwt中间件
+const secret = 'hit'
+
+
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function (req, res, next) {
+  res.send("respond with a resource");
 });
 
-router.get('/test', function(req, res, next) {
+router.get("/test", function (req, res, next) {
   res.json({
-    code:0,
-    data:{
-      name:'hit'
+    code: 0,
+    data: {
+      name: "hit"
     },
-    msg:'成功返回'
-  })
+    msg: "成功返回"
+  });
 });
 
+
+router.post("/login", function (req, res, next) {
+  let user = req.body;
+  var expires = Date.now() + 7 * 24 * 60 * 60 * 1000;
+  let token = jwt.encode({//编码
+    id: user.username,
+    username: user.password
+  }, secret);
+  res.json({
+    code: 0,
+    data: {
+      name: user.username,
+      password: user.password,
+      token: token,
+      expires: expires
+    },
+    msg: "成功返回"
+  });
+});
+
+// 用户校验 中间件
+let auth = function(req, res, next){
+  //post模拟时 添加Headers Authorization: Bearer token的值
+  let authorization = req.headers['authorization']
+  if(authorization) {
+      let token = authorization.split(' ')[1];
+      try {
+          //看token是否合法，解码，如果串改过token就解不出来,进入异常页面
+          let user = jwt.decode(token, secret);
+          req.user = user;//后面就可以拿到user，中间件用法 
+          next();//下一步
+      } catch (error) {
+          console.log(error)
+          res.status(401).send('Not Allowed')
+      }
+  } else {
+      res.status(401).send('Not Allowed');
+  }
+}
+
+
+router.get('/order', auth, function(req,res,next){
+  res.json({
+      code: 0,
+      data: {
+          user: req.user
+      }
+  })
+})
 module.exports = router;
